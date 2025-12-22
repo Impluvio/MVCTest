9c597d870216f5bc70951690815429036fe0ec45
Adam Tuffey <adamcorneliustuffey@gmail.com>
2025-12-22 23:21:29 +0000

    Initial Commit Db Context & Connection setup

---
+++ MVCtest/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCtest.Data
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext()
+        {
+       
+        }
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
